1a176b3ca1d2b8276cc0682d87ded950e91ed3d8
vcasteline <vcasteline@ufl.edu>
2024-07-01 03:57:13 -0400

    change css import datepicker

--- src/components/BirthdayField/BirthdayField.js
+++ src/components/BirthdayField/BirthdayField.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
+import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import css from './BirthdayField.module.css'
 
 const BirthdayField = ({ input, meta, intl, formId }) => {
