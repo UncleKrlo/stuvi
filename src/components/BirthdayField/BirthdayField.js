4d5b0b7abf326f9d414c6620563ff74e0cbdc5d8
“UncleKrlo” <carlos@stuvi.space>
2024-07-02 23:17:10 -0400

    removed calendar packages, added normal date input

--- src/components/BirthdayField/BirthdayField.js
+++ src/components/BirthdayField/BirthdayField.js
@@ -1,46 +1,27 @@
-import React from 'react';
-// import DatePicker from 'react-datepicker';
-// import './ReactDatePicker.module.css'
-import css from './BirthdayField.module.css'
+import React, { useState } from 'react';
+import css from './BirthdayField.module.css';
 
 const BirthdayField = ({ input, meta, intl, formId }) => {
-  const minDate = new Date();
-  minDate.setFullYear(minDate.getFullYear() - 18); // Edad mínima de 18 años
+  const [date, setDate] = useState(input.value || '');
 
-  const handleChange = (date) => {
-    if (date) {
-      const adjustedDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-      const formattedDate = adjustedDate.toISOString().split('T')[0];
-      input.onChange(formattedDate);
-    } else {
-      input.onChange(null);
-    }
+  const handleChange = e => {
+    const newDate = e.target.value;
+    setDate(newDate);
+    input.onChange(newDate);
   };
-  const parseDate = (dateString) => {
-    if (dateString) {
-      const [year, month, day] = dateString.split('-');
-      return new Date(year, month - 1, day);
-    }
-    return null;
-  };
-
 
   return (
     <div className={css.birthdayField}>
       <label htmlFor={formId ? `${formId}.birthday` : 'birthday'}>
         {intl.formatMessage({ id: 'SignupForm.birthdayLabel' })}
       </label>
-      {/* <DatePicker
-        className={css.datePicker} 
+      <input
+        type="date"
         id={formId ? `${formId}.birthday` : 'birthday'}
-        value={input.value ? parseDate(input.value) : null}
+        value={date}
         onChange={handleChange}
-        maxDate={minDate}
-        showYearDropdown
-        scrollableYearDropdown
-        yearDropdownItemNumber={100}
-        placeholderText={intl.formatMessage({ id: 'SignupForm.birthdayPlaceholder' })}
-      /> */}
+        max={new Date().toISOString().split('T')[0]} // Establece la fecha máxima como hoy
+      />
       {meta.error && meta.touched && <span className={css.error}>{meta.error}</span>}
     </div>
   );
